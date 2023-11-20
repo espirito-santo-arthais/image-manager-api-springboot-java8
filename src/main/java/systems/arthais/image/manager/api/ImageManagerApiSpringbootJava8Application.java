fdf1c04fc89e23baa20c2eb843fa8baeba76c58b
Raimundo do Espírito Santo <espirito.santo.arthais@outlook.com>
2023-11-20 19:26:54 -0300

    Carga inicial

---
+++ src/main/java/systems/arthais/image/manager/api/ImageManagerApiSpringbootJava8Application.java
@@ -0,0 +1,13 @@
+package systems.arthais.image.manager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageManagerApiSpringbootJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageManagerApiSpringbootJava8Application.class, args);
+	}
+
+}
---
+++ src/test/java/systems/arthais/image/manager/api/ImageManagerApiSpringbootJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package systems.arthais.image.manager.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageManagerApiSpringbootJava8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
